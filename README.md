7a89d03c1b42b9fca5325cd26d7e7de2e00d0bc1
Artemy <joggsaronus@gmail.com>
2024-12-14 23:15:12 +0500

    added files analysis, filled README

---
+++ README.md
@@ -0,0 +1,19 @@
+# URL and file analysis Telegram bot (VirusTotal)
+
+## Описание
+Telegram бот, который умеет обрабывать ссылки и файлы через VirusTotal API. Если по объекту есть подозрения - бот сообщит информацию о количестве антивирусных движков, которые посчитали объект небезопасным.
+
+## Подготовка окружения для разработки (Windows)
+- python -m venv .venv
+- .venv/Scripts/activate
+- pip install -r requirements.txt
+
+## Запуск приложения локально (без Docker)
+- python run.py
+
+## Запуск приложения локально (с Docker)
+- docker compose up --build
+
+## Остановка и очистка
+- остановка командой `docker compose down`
+- после нескольких тестовых сборок можно почистить образы/контейнеры которые накопились: `docker system prune`
--- app/handlers.py
+++ app/handlers.py
@@ -1,4 +1,6 @@
 import requests
+import time
+from io import BytesIO
 from aiogram import Router, F
 from aiogram.filters import CommandStart
 from aiogram.types import Message, ContentType
@@ -17,6 +19,8 @@ def scan_url(url: str) -> str:
     )
     result = response.json()
 
+    time.sleep(5)
+
     if response.status_code == 200:
         analysis_id = result["data"]["id"]
         analysis_response = requests.get(
@@ -30,9 +34,6 @@ def scan_url(url: str) -> str:
             return (
                 (
                     f"✅ <b>Всё в порядке, ни один из антивирусов не посчитал эту ссылку подозрительной</b>\n\n"
-                    # f"✅ Безопасно: {stats['harmless']}\n\n"
-                    # f"🤔 Подозрительно: {stats['suspicious']}\n\n"
-                    # f"⛔️ Вредоносно: {stats['malicious']}\n\n"
                 )
                 if stats["suspicious"] == 0 and stats["malicious"] == 0
                 else (
@@ -49,12 +50,50 @@ def scan_url(url: str) -> str:
         return f"🤖 <b>Ошибка при проверке ссылки, проверьте правильность введённой ссылки</b>"
 
 
+def scan_file(file: BytesIO, file_name: str) -> str:
+    """Сканирование файла."""
+    headers = {"x-apikey": VIRUSTOTAL_API_KEY}
+    files = {"file": (file_name, file)}
+    response = requests.post(f"{VIRUSTOTAL_URL}/files", headers=headers, files=files)
+    result = response.json()
+
+    time.sleep(5)
+
+    if response.status_code == 200:
+        analysis_id = result["data"]["id"]
+        analysis_response = requests.get(
+            f"{VIRUSTOTAL_URL}/analyses/{analysis_id}", headers=headers
+        )
+        analysis_result = analysis_response.json()
+
+        if analysis_response.status_code == 200:
+            stats = analysis_result["data"]["attributes"]["stats"]
+
+            return (
+                (
+                    f"✅ <b>Всё в порядке, ни один из антивирусов не посчитал этот файл подозрительным</b>\n\n"
+                )
+                if stats["suspicious"] == 0 and stats["malicious"] == 0
+                else (
+                    f"❗️<b>Не все антивирусы посчитали файл безопасным</b>\n\n"
+                    f"✅ Безопасно: {stats['harmless']}\n\n"
+                    f"🤔 Подозрительно: {stats['suspicious']}\n\n"
+                    f"⛔️ Вредоносно: {stats['malicious']}\n\n"
+                )
+            )
+
+        else:
+            return f"Ошибка при получении результатов анализа: {analysis_result.get('error', 'Неизвестная ошибка')} {response.status_code}"
+    else:
+        return f"🤖 <b>Ошибка при проверке файла, проверьте правильность введённого файла</b>"
+
+
 @router.message(CommandStart())
 async def command_start_handler(message: Message):
     """Обработчик команды /start."""
     await message.answer(
         f"👋 Приветствую, {message.from_user.full_name}! "
-        f"Отправьте ссылку для проверки."
+        f"Отправьте ссылку или файл для проверки."
     )
 
 
@@ -62,12 +101,18 @@ async def command_start_handler(message: Message):
 async def handle_link(message: Message):
     """Обработка строки-ссылки."""
     link = message.text.strip()
-    # Проверяем, есть ли протокол в начале
-    if not link.startswith(("http://", "https://", "ftp://")):
-        # Не добавляем ничего, если нет протокола
-        result = scan_url(link)
-    else:
-        # Ссылка уже содержит протокол
-        result = scan_url(link)
+    # Удаляем протокол, если он есть
+    link = link.replace("http://", "").replace("https://", "").replace("ftp://", "")
+    result = scan_url(link)
+    await message.answer(result, parse_mode="HTML")
 
+
+@router.message(F.content_type == ContentType.DOCUMENT)
+async def handle_document(message: Message):
+    """Обработка документа."""
+    document = message.document
+    file_info = await message.bot.get_file(document.file_id)
+    file = await message.bot.download_file(file_info.file_path)
+    file_bytes = BytesIO(file.read())
+    result = scan_file(file_bytes, document.file_name)
     await message.answer(result, parse_mode="HTML")
